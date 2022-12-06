d1803b13c721f1ce6c6dbec9f9a40bd824b43479
Pityken <pityken@gmail.com>
2022-12-06 11:24:39 +0100

    start implementing MapLoader

---
+++ Codecool.MarsExploration.MapExplorer/MapLoader/MapLoader.cs
@@ -0,0 +1,22 @@
+﻿using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.MapLoader;
+
+internal class MapLoader : IMapLoader
+{
+    public Map Load(string mapFile)
+    {
+        var rawMap = File.ReadAllLines(mapFile);
+        var dimension = rawMap.Length;
+        var loadedMap = new string[dimension,dimension];
+        for(var i = 0; i < dimension; i++)
+        {
+            rawMap[i].Select(x => loadedMap[i,rawMap[i].IndexOf(x)]);
+        }
+        foreach(var line in rawMap) 
+        {
+            line.Select(x => loadedMap[Array.IndexOf(rawMap, line), line.IndexOf(x)]);
+        }
+        return new Map(loadedMap, true);
+    }
+}

79228874a5009965fe9b0909bc9d1a127fd54f64
Akos <barcza.akos15@gmail.com>
2022-12-06 10:33:05 +0100

    start to implement Rover Deployer

---
+++ Codecool.MarsExploration.MapExplorer/MarsRover/MarsRoverDeployer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.MarsRover
+{
+    public class MarsRoverDeployer
+    {
+        public void Deploy(Map map, int[,] coordinates)
+        {
+            //if (!canDeploy) return;
+            
+        }
+    }
+
+}
+
+

22bfaf2b1560013904986fee81fec7315aedacbd
Akos <barcza.akos15@gmail.com>
2022-12-07 20:33:24 +0100

    add rover and explorationSimulator to Program.cs

--- Codecool.MarsExploration.MapExplorer/Program.cs
+++ Codecool.MarsExploration.MapExplorer/Program.cs
@@ -1,7 +1,11 @@
 ﻿using Codecool.MarsExploration.MapExplorer.Configuration.Provider;
 using Codecool.MarsExploration.MapExplorer.Configuration.Service;
+using Codecool.MarsExploration.MapExplorer.Exploration;
 using Codecool.MarsExploration.MapExplorer.MapLoader;
 using Codecool.MarsExploration.MapExplorer.MarsRover;
+using Codecool.MarsExploration.MapExplorer.Simulation.Model;
+using Codecool.MarsExploration.MapExplorer.Simulation.Service;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
 using Codecool.MarsExploration.MapGenerator.MapElements.Model;
 
 namespace Codecool.MarsExploration.MapExplorer;
@@ -32,8 +36,7 @@ internal class Program
         IMineralListProvider mineralListProvider = new MineralListProvider();
         IStartingCoordinateProvider startingCoordinateProvider = new StartingCoordinateProvider();
         IRoverConfigurationProvider roverConfigurationProvider = new RoverConfigurationProvider();
-
-
+        ExplorationOutcome Outcome = new ExplorationOutcome();
         var landingSpot = startingCoordinateProvider.GetStartingCoordinate(row, col);
 
 
@@ -41,10 +44,13 @@ internal class Program
 
         var mineralGoals = mineralListProvider.GetMinerals(possibleMinerals, amountToGather);
         var roverConfiguration = roverConfigurationProvider.GetRoverConfiguration(mapFile, landingSpot, mineralGoals, simulationSteps);
+        MarsRover.MarsRover Rover = new MarsRover.MarsRover(1, roverConfiguration.startingCoordinate, 5, new List<Coordinate>(){});
         IRoverConfigurationValidator roverConfigrationValidator = new RoverConfigurationValidator(_obstacles);
 
         var roverConfigurationIsValid = roverConfigrationValidator.Validate(roverConfiguration);
         roverDeployer.Deploy(roverConfigurationIsValid, roverConfiguration, loadedMap);
+        SimulationContext Context = new SimulationContext(1, 300, Rover, Rover.currentPosition, loadedMap, possibleMinerals, Outcome, roverConfiguration);
 
+        IExplorationSimulator explorationSimulator = new ExplorationSimulator(Context);
     }
 }

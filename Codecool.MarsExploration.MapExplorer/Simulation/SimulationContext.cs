9205c65b9bed271bf852a7c2a0e968b29f3c72e7
Akos <barcza.akos15@gmail.com>
2022-12-07 12:30:13 +0100

    implement SimulationContext record

---
+++ Codecool.MarsExploration.MapExplorer/Simulation/SimulationContext.cs
@@ -0,0 +1,10 @@
+﻿using Codecool.MarsExploration.MapExplorer.MarsRover;
+using Codecool.MarsExploration.MapExplorer.Configuration.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+using Codecool.MarsExploration.MapExplorer.Exploration;
+
+namespace Codecool.MarsExploration.MapExplorer.Simulation
+{
+    public record SimulationContext(int Steps, int StepLimit, MarsRover.MarsRover Rover, Coordinate StartingCoordinates, Map map, List<string> ResourceSymbols, ExplorationOutcome Outcome);
+}

fce2abba04d003d33bfeccef94a0874ce408a166
Akos <barcza.akos15@gmail.com>
2022-12-07 12:35:22 +0100

    move into Model folder

--- Codecool.MarsExploration.MapExplorer/Simulation/Model/SimulationContext.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Model/SimulationContext.cs
@@ -4,7 +4,7 @@ using Codecool.MarsExploration.MapGenerator.Calculators.Model;
 using Codecool.MarsExploration.MapGenerator.MapElements.Model;
 using Codecool.MarsExploration.MapExplorer.Exploration;
 
-namespace Codecool.MarsExploration.MapExplorer.Simulation
+namespace Codecool.MarsExploration.MapExplorer.Simulation.Model
 {
     public record SimulationContext(int Steps, int StepLimit, MarsRover.MarsRover Rover, Coordinate StartingCoordinates, Map map, List<string> ResourceSymbols, ExplorationOutcome Outcome);
 }
